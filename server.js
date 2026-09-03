d4512e09e85e94de6e794e74634e5735f7b79b8b
Sebastian Fernandez <sfernand@gmail.com>
2026-09-03 16:54:35 -0400

    use variable

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const authRoutes = require('./routes/authRoutes');
 const app = express();
 app.use(express.json());
 
-// Reemplaza <credentials> con tu usuario y contraseña de MongoDB
-mongoose.connect('mongodb://<credentials>@153.75.224.130:5432/cars?directConnection=true&authSource=admin')
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Conectado a MongoDB'))
   .catch(err => console.error('Error al conectar:', err));
 
 app.use('/api/auth', authRoutes);
 
-app.listen(3009, () => console.log('Servidor corriendo en el puerto 3009'));
+app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'));
+
