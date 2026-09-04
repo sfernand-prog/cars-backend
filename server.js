9467d7fd9f384fa2b87f7fd5e11480e94c4e4e99
Sebastian Fernandez <sfernand@gmail.com>
2026-09-04 17:16:29 -0400

    package.json missing

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
+const autoRoutes = require('./routes/autoRoutes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
@@ -10,6 +13,6 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.error('Error al conectar:', err));
 
 app.use('/api/auth', authRoutes);
+app.use('/api/autos', autoRoutes);
 
-app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'));
-
+app.listen(3009, () => console.log('Servidor corriendo en el puerto 3009'));
