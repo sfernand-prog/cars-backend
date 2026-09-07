119fb1334768e8c188f244573445462e2278cc9b
Sebastian Fernandez <sfernand@gmail.com>
2026-09-07 18:07:18 -0300

    new logic

--- routes/autoRoutes.js
+++ routes/autoRoutes.js
@@ -2,20 +2,27 @@ const express = require('express');
 const router = express.Router();
 const Auto = require('../models/Auto');
 
-// Obtener todos los autos
 router.get('/', async (req, res) => {
   const autos = await Auto.find();
   res.json(autos);
 });
 
-// Obtener un auto por patente
+router.post('/', async (req, res) => {
+  try {
+    const nuevoAuto = new Auto(req.body);
+    await nuevoAuto.save();
+    res.status(201).json(nuevoAuto);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al crear el auto' });
+  }
+});
+
 router.get('/:patente', async (req, res) => {
   const auto = await Auto.findOne({ Patente: req.params.patente });
   if (!auto) return res.status(404).json({ error: 'Auto no encontrado' });
   res.json(auto);
 });
 
-// Actualizar un auto
 router.put('/:patente', async (req, res) => {
   try {
     const auto = await Auto.findOneAndUpdate({ Patente: req.params.patente }, req.body, { new: true });
